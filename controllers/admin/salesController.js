7f99aec722f0890c2f53407caba0cef943f66e39
Fawas <fawasalikariyeri@gmail.com>
2025-08-09 00:11:35 +0530

    sales report generation fix

--- controllers/admin/salesController.js
+++ controllers/admin/salesController.js
@@ -126,10 +126,12 @@ const generatePDF = async (res, salesData) => {
     );
 
     
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
     const page = await browser.newPage();
 
-   
     await page.setContent(html, { waitUntil: 'networkidle0' });
 
     
